7fd17f2ac790d69dcb696932c8273c16c58e152b
jmartinii <j_martinii@yahoo.com>
2025-03-25 03:03:13 -0800

    Draft2

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
 
     running = True
     while running:
-        screen.fill(black)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill(black)
+        pygame.display.flip()
+        dt = clock.tick(60 / 1000)
 
 
 
